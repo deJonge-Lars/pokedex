235a3a4cf402d26dbddf33a0f4091361dc6407bd
deJonge-Lars <62877995+deJonge-Lars@users.noreply.github.com>
2021-02-13 21:08:35 +0100

    Updating App
    Trying to load the images from the GitHub, currently mostly: To Many Requests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dasboard from './components/layout/Dasboard';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <NavBar />
       <div className="container">
         <Dasboard />
--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,16 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import styled from 'styled-components';
+
+import spinner from '../pokemon/spinner.gif';
+
+const Sprite = styled.img`
+    width: 5em;
+    height: 5em;
+    display: none;
+`;
+
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.2);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover {
+        box-shadow 0 10px, 20px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.25);
+    }
+`;
+
 
 export default class PokemonCard extends Component {
     state ={
         name: '',
         imageUrl: '',
-        pokemonIndex: ''
+        pokemonIndex: '',
+        imageLoading: true,
+        toManyRequests: false
     };
 
     componentDidMount() {
         const { name, url } = this.props;
         const pokemonIndex = url.split("/")[url.split('/').length - 2];
-        const imageUrl = `https://github.com/PokiAPI/sprits/blob/master/sprites/pokemon${pokemonIndex}.png?raw=true`
+        const imageUrl = `https://github.com/PokiAPI/sprites/blob/master/sprites/pokemon${pokemonIndex}.png?raw=true`
         
         this.setState({ name, imageUrl, pokemonIndex });
 
@@ -19,11 +40,40 @@ export default class PokemonCard extends Component {
     render() {
         return (
             <div className='col-md-3 col-sm-6 mb-5'>
-                <div className="card">
-                    <div className="card-header">
-                        <h1>{this.state.name}</h1>
-                    </div>
-                </div>
+                <Card className="card">
+                    <h5 className="card-header">{this.state.pokemonIndex}</h5>
+                    {this.state.imageLoading ? (
+                        <img src={spinner}
+                            style={{width: '5em', height: '5em'}}
+                            className="card-img-top rounded mx-auto d-block mt-2" 
+                            alt=""></img>
+                    ) : null}
+                    <Sprite className="card-img-top rounded mx-auto mt-2"
+                      onLoad={() => this.setState({ imageLoading: false })}
+                      onError={() => this.setState({ toManyRequests: true })}
+                      src={this.state.imageUrl}
+                      style={
+                          this.state.toManyRequests ? { display: "none" } :
+                          this.state.imageLoading ? null : { display: "block" }
+                      }
+                    />
+                    {this.state.toManyRequests ? (
+                      <h6 className='mx-auto'>
+                        <span className="badge badge-danger mt-2">To Many Requests</span>
+                      </h6>
+                    ) : null}
+                     <div className="card-body mx-auto">
+                        <h6 className="card-title">
+                            {this.state.name
+                            .toLowerCase()
+                            .split(" ")
+                            .map(
+                                letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                            )
+                            .join(' ')}
+                        </h6>
+                    </div> 
+                </Card>
             </div>
         );
     }
