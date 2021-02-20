b45d98fdfb3713df2e1f459ce2b3bae1a38490da
deJonge-Lars <62877995+deJonge-Lars@users.noreply.github.com>
2021-02-20 12:01:50 +0100

    Updating Pokelist
    Added full first gen (151) pokemons to the list and removed the ability which was an error and displayed the ability names from the pokemons instead of the pokemon names

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -5,7 +5,7 @@ import PokemonCard from './PokemonCard';
 
 export default class PokemonList extends Component {
     state= {
-        url: "https://pokeapi.co/api/v2/ability/?limit151&offset=0/",
+        url: "https://pokeapi.co/api/v2/pokemon/?limit=151",
         pokemon: null
     };
 
